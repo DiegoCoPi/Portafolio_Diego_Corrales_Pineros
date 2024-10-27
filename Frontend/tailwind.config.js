a3d874db2dc7baa46fed44dd8bceabd3f5a1d461
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2024-10-26 22:09:03 -0500

    Avance 26*10-2024

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*.html", 
     "./src/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
       backgroundImage:{
-        'custom-image': "url('/src/imagenes/inteligencia.jpg')"
+        'text-gradient':"linear-gradient(45deg, #ff7e5f, #feb47b)",
+        'custom-image':"url('./src/imagenes/inteligencia.jpg')",
+        'photo-image':"url('')"
       }  
     },
   },
