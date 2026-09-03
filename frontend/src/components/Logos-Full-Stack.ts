8c7ee86c8c8ce3a1d0ab1248b08a4385aedd91ea
DiegoCoPi <diegoalexander.cp@gmail.com>
2026-09-02 22:20:37 -0500

    Avance 2 02-09-2026

---
+++ frontend/src/components/Logos-Full-Stack.ts
@@ -0,0 +1,23 @@
+// Definiendo la interfaz
+interface Logos {
+    id: number,
+    name: string
+    alt: string
+    img: string
+}
+
+const DevLogos = [
+    { id: 1, name: "JavaScript", alt: "JavaScript Logo" },
+    { id: 2, name: "MySQL", alt: "MySQL Logo" },
+    { id: 3, name: "NestJS", alt: "NestJS Logo" },
+    { id: 4, name: "Node.js", alt: "Node.js Logo" },
+    { id: 5, name: "React", alt: "React Logo" },
+]
+
+
+export const LogosFullStack: Logos[] = DevLogos.map((tech) => ({
+    id: tech.id,
+    name: tech.name,
+    alt: tech.alt,
+    img: new URL(`@/assets/Programming/${tech.name}.png`, import.meta.url).href
+}))
